7beb67480e2a01b2855eaa966ae5c1c94b5ac6d2
Joachim <joachim.stolle@hotmail.de>
2022-04-01 14:39:38 +0200

    Add OrderRepo class

---
+++ src/main/java/jst/shop/OrderRepo.java
@@ -0,0 +1,65 @@
+package jst.shop;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+public class OrderRepo {
+    List<Order> orderList = new ArrayList<>();
+
+    public OrderRepo() {
+    }
+
+    public void addOrder() {
+        if (orderList.isEmpty()) {
+            orderList.add(new Order(1));
+        } else {
+            orderList.add(new Order(orderList.get(orderList.size() - 1).getOrderID()+ 1));
+        }
+    }
+
+    public Order getOrder(int id) throws NoSuchElementException {
+        for (Order order: orderList) {
+            if (order.getOrderID() == id) {
+                return order;
+            }
+        }
+        throw new NoSuchElementException();
+    }
+
+    public List<Order> getOrderList() {
+        return orderList;
+    }
+
+
+    public String toStringToDisplay() {
+        String result = "";
+        for (Order order: orderList) {
+            result += "\n\n" + order.toStringToDisplay();
+        }
+
+
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "OrderRepo{" +
+                "orderList=" + orderList +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderRepo orderRepo = (OrderRepo) o;
+        return Objects.equals(orderList, orderRepo.orderList);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(orderList);
+    }
+}
