11c55efadfac0308c004dc625b84f9caac658e0d
Joachim <joachim.stolle@hotmail.de>
2022-04-01 10:26:22 +0200

    Add Product class

---
+++ src/main/java/jst/shop/Product.java
@@ -0,0 +1,34 @@
+package jst.shop;
+
+public class Product {
+    private int productID;
+    private String productName;
+
+    public Product(int productID, String productName) {
+        this.productID = productID;
+        this.productName = productName;
+    }
+
+
+
+
+    public int getProductID() {
+        return productID;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public String toStringToDisplay() {
+        return "\nProduktname: " + productName + "\nProduktnummer: " + productID;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "productID=" + productID +
+                ", productName='" + productName + '\'' +
+                '}';
+    }
+}
